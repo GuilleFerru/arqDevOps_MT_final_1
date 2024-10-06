5f638f4cf6c52a8fc5cadba84877960112d46881
GuilleFerru <guillelf@gmail.com>
2024-10-06 13:38:11 -0300

    terraform config

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,3 +1,12 @@
-resource "linode_instance" "jenkins" {
+# resources.tf
+
+resource "aws_instance" "jenkins" {
+  ami           = "ami-0075013580f6322a1"
+  instance_type = "t3.micro"
+  key_name      = "aws-keypair"
+
+  tags = {
+    Name = "jenkins"
+  }
 
 }
